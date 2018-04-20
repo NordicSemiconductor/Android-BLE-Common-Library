58079c768121b63fd662d6ca959bb3c39e678317
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-04-20 22:52:08 +0200

    Continuous Glucose Measurement data callback

--- ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/ContinuousGlucoseMeasurementDataCallback.java
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/ContinuousGlucoseMeasurementDataCallback.java
@@ -5,12 +5,104 @@ import android.support.annotation.NonNull;
 
 import no.nordicsemi.android.ble.callback.profile.ProfileDataCallback;
 import no.nordicsemi.android.ble.common.profile.ContinuousGlucoseMeasurementCallback;
+import no.nordicsemi.android.ble.common.util.CRC16;
 import no.nordicsemi.android.ble.data.Data;
 
+@SuppressWarnings("ConstantConditions")
 public abstract class ContinuousGlucoseMeasurementDataCallback implements ProfileDataCallback, ContinuousGlucoseMeasurementCallback {
 
 	@Override
 	public void onDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+		int offset = 0;
 
+		while (offset < data.size()) {
+			// Packet size
+			final int size = data.getIntValue(Data.FORMAT_UINT8, offset);
+
+			if (size < 6 || offset + size > data.size()) {
+				onInvalidDataReceived(device, data);
+				return;
+			}
+
+			// Flags
+			final int flags = data.getIntValue(Data.FORMAT_UINT8, offset + 1);
+
+			final boolean cgmTrendInformationPresent   = (flags & 0x01) != 0;
+			final boolean cgmQualityInformationPresent = (flags & 0x02) != 0;
+			final boolean sensorWarningOctetPresent    = (flags & 0x20) != 0;
+			final boolean sensorCalTempOctetPresent    = (flags & 0x40) != 0;
+			final boolean sensorStatusOctetPresent     = (flags & 0x80) != 0;
+
+			final int dataSize = 6 + (cgmTrendInformationPresent ? 2 : 0) + (cgmQualityInformationPresent ? 2 : 0)
+					+ (sensorWarningOctetPresent ? 1 : 0) + (sensorCalTempOctetPresent ? 1 : 0)
+					+ (sensorStatusOctetPresent ? 1 : 0);
+
+			if (size != dataSize && size != dataSize + 2) {
+				onInvalidDataReceived(device, data);
+				return;
+			}
+
+			final boolean crcPresent = size == dataSize + 2;
+			if (crcPresent) {
+				final int expectedCrc = data.getIntValue(Data.FORMAT_UINT16, offset + dataSize);
+				final int actualCrc = CRC16.MCRF4XX(data.getValue(), offset, dataSize);
+				if (expectedCrc != actualCrc) {
+					onCrcError(data);
+					return;
+				}
+			}
+
+			offset += 2;
+			// Glucose concentration
+			final float glucoseConcentration = data.getFloatValue(Data.FORMAT_SFLOAT, offset);
+			offset += 2;
+
+			// Time offset (in minutes since Session Start)
+			final int timeOffset = data.getIntValue(Data.FORMAT_UINT16, offset);
+			offset += 2;
+
+			// Sensor Status Annunciation
+			int warningStatus = 0;
+			int calibrationTempStatus = 0;
+			int sensorStatus = 0;
+			Status status = null;
+
+			if (sensorWarningOctetPresent) {
+				warningStatus = data.getIntValue(Data.FORMAT_UINT8, offset++);
+			}
+			if (sensorCalTempOctetPresent) {
+				calibrationTempStatus = data.getIntValue(Data.FORMAT_UINT8, offset++);
+			}
+			if (sensorStatusOctetPresent) {
+				sensorStatus = data.getIntValue(Data.FORMAT_UINT8, offset++);
+			}
+			if (sensorWarningOctetPresent || sensorCalTempOctetPresent || sensorStatusOctetPresent) {
+				status = new Status(warningStatus, calibrationTempStatus, sensorStatus);
+			}
+
+			// CGM Trend Information
+			Float trend = null;
+			if (cgmTrendInformationPresent) {
+				trend = data.getFloatValue(Data.FORMAT_SFLOAT, offset);
+				offset += 2;
+			}
+
+			// CGM Quality Information
+			Float quality = null;
+			if (cgmQualityInformationPresent) {
+				quality = data.getFloatValue(Data.FORMAT_SFLOAT, offset);
+				offset += 2;
+			}
+
+			// E2E-CRC
+			if (crcPresent) {
+				offset += 2;
+			}
+
+			onContinuousGlucoseMeasurementReceived(glucoseConcentration, trend, quality, timeOffset);
+			if (status != null) {
+				onSensorStatusChanged(status, timeOffset);
+			}
+		}
 	}
 }
---
+++ ble-common/src/test/java/no/nordicsemi/android/ble/common/callback/ContinuousGlucoseMeasurementDataCallbackTest.java
@@ -0,0 +1,275 @@
+package no.nordicsemi.android.ble.common.callback;
+
+import android.bluetooth.BluetoothDevice;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.junit.Test;
+
+import no.nordicsemi.android.ble.callback.DataCallback;
+import no.nordicsemi.android.ble.common.util.CRC16;
+import no.nordicsemi.android.ble.data.Data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class ContinuousGlucoseMeasurementDataCallbackTest {
+
+	@Test
+	public void onContinuousGlucoseMeasurementReceived_full() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Glucose", 12.34, glucoseConcentration, 0.01);
+				assertNotNull("Trend present", cgmTrend);
+				assertEquals("Trend", 0.2f, cgmTrend, 0.01);
+				assertNotNull("Quality present", cgmQuality);
+				assertEquals("Quality", 99.7f, cgmQuality, 0.01);
+				assertEquals("Time offset", 5, timeOffset);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertNotNull("Status present", status);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				final int expectedCrc = data.getIntValue(Data.FORMAT_UINT16, 13);
+				final int actualCrc   = CRC16.MCRF4XX(data.getValue(), 0, 13);
+				assertEquals("CRC error on valid data", expectedCrc, actualCrc);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Correct data reported as invalid", 1, 2);
+			}
+		};
+		final Data data = new Data(new byte[15]);
+		// Size
+		assertTrue(data.setValue(15, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b11100011, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(1234, -2, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertTrue(data.setValue(5, Data.FORMAT_UINT16, 4));
+		// Status
+		assertTrue(data.setByte(0x02, 6)); // Warning status: Low battery
+		assertTrue(data.setByte(0x14, 7)); // Cal/Temp status: Sensor Temperature too high / Calibration recommended
+		assertTrue(data.setByte(0xFF, 8)); // Sensor status: All fields
+		// Trend
+		assertTrue(data.setValue(2, -1, Data.FORMAT_SFLOAT, 9));
+		// Quality
+		assertTrue(data.setValue(997, -1, Data.FORMAT_SFLOAT, 11));
+		// E2E CRC
+		assertTrue(data.setValue(0x3F8E, Data.FORMAT_UINT16, 13));
+
+		callback.onDataReceived(null, data);
+	}
+
+	@Test
+	public void onContinuousGlucoseMeasurementReceived_small() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Glucose", 120.0, glucoseConcentration, 0.01);
+				assertNull("Trend not present", cgmTrend);
+				assertNull("Quality not present", cgmQuality);
+				assertEquals("Time offset", 6, timeOffset);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertEquals("Status not present but reported", 1, 2);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				assertEquals("CRC error on valid data", 1, 2);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Correct data reported as invalid", 1, 2);
+			}
+		};
+		final Data data = new Data(new byte[6]);
+		// Size
+		assertTrue(data.setValue(6, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b00000000, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertTrue(data.setValue(6, Data.FORMAT_UINT16, 4));
+
+		callback.onDataReceived(null, data);
+	}
+
+	@Test
+	public void onContinuousGlucoseMeasurementReceived_double() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			private int time = 5;
+
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Glucose", 120.0, glucoseConcentration, 0.01);
+				assertNull("Trend not present", cgmTrend);
+				assertNull("Quality not present", cgmQuality);
+				assertEquals("Time offset", time++, timeOffset);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertEquals("Status not present but reported", 1, 2);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				assertEquals("CRC error on valid data", 1, 2);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Correct data reported as invalid", 1, 2);
+			}
+		};
+		final Data data = new Data(new byte[12]);
+		// Size
+		assertTrue(data.setValue(6, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b00000000, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertTrue(data.setValue(5, Data.FORMAT_UINT16, 4));
+
+		// Size
+		assertTrue(data.setValue(6, Data.FORMAT_UINT8, 6));
+		// Flags
+		assertTrue(data.setByte(0b00000000, 7));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 8));
+		// Time offset
+		assertTrue(data.setValue(6, Data.FORMAT_UINT16, 10));
+
+		callback.onDataReceived(null, data);
+	}
+
+	@Test
+	public void onContinuousGlucoseMeasurementReceived_crcError() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Measurement reported despite wrong CRC", 1, 2);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertEquals("Status reported despite wrong CRC", 1, 2);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				final int actualCrc = CRC16.MCRF4XX(data.getValue(), 0, 6);
+				assertEquals("CRC error", 0x6F59, actualCrc);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Correct data reported as invalid", 1, 2);
+			}
+		};
+		final Data data = new Data(new byte[8]);
+		// Size
+		assertTrue(data.setValue(8, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b00000000, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertTrue(data.setValue(6, Data.FORMAT_UINT16, 4));
+		// E2E CRC
+		assertTrue(data.setValue(0x6F58, Data.FORMAT_UINT16, 6));
+
+		callback.onDataReceived(null, data);
+	}
+
+	@Test
+	public void onInvalidDataReceived_tooShort() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Measurement reported despite invalid data", 1, 2);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertEquals("Status reported despite invalid data", 1, 2);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				assertEquals("Invalid data reported as CRC error", 1, 2);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Invalid data", 1, 1);
+			}
+		};
+		final Data data = new Data(new byte[5]);
+		// Size
+		assertTrue(data.setValue(6, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b11100011, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertFalse(data.setValue(6, Data.FORMAT_UINT16, 4));
+
+		callback.onDataReceived(null, data);
+	}
+
+	@Test
+	public void onInvalidDataReceived() {
+		final DataCallback callback = new ContinuousGlucoseMeasurementDataCallback() {
+			@Override
+			public void onContinuousGlucoseMeasurementReceived(final float glucoseConcentration, @Nullable final Float cgmTrend, @Nullable final Float cgmQuality, final int timeOffset) {
+				assertEquals("Measurement reported despite invalid data", 1, 2);
+			}
+
+			@Override
+			public void onSensorStatusChanged(final Status status, final int timeOffset) {
+				assertEquals("Status reported despite invalid data", 1, 2);
+			}
+
+			@Override
+			public void onCrcError(@NonNull final Data data) {
+				assertEquals("Invalid data reported as CRC error", 1, 2);
+			}
+
+			@Override
+			public void onInvalidDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+				assertEquals("Invalid data", 1, 1);
+			}
+		};
+		final Data data = new Data(new byte[7]);
+		// Size
+		assertTrue(data.setValue(7, Data.FORMAT_UINT8, 0));
+		// Flags
+		assertTrue(data.setByte(0b11100011, 1));
+		// Glucose Concentration
+		assertTrue(data.setValue(12, 1, Data.FORMAT_SFLOAT, 2));
+		// Time offset
+		assertTrue(data.setValue(6, Data.FORMAT_UINT16, 4));
+		// Trend
+		assertFalse(data.setValue(2, -1, Data.FORMAT_SFLOAT, 9));
+
+		callback.onDataReceived(null, data);
+	}
+}
