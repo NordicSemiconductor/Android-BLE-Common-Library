e14ceebf9f640800eb574f4645ac61a4f9ebbb66
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-04-20 21:39:51 +0200

    More CRC-16 algorithms

--- ble-common/src/main/java/no/nordicsemi/android/ble/common/util/CRC16.java
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/util/CRC16.java
@@ -21,24 +21,10 @@ public final class CRC16 {
 	 * @param data   The input data block for computation.
 	 * @param offset Offset from where the range starts.
 	 * @param length Length of the range in bytes.
-	 * @return the CRC-16 CCITT (Kermit_.
+	 * @return the CRC-16 CCITT (Kermit).
 	 */
 	public static int CCITT_Kermit(final @NonNull byte[] data, final int offset, final int length) {
-		final int polynomial = 0x8408; // reversed 0x1021
-		int crc = 0x0000;
-
-		for (int index = offset; index < offset + length && index < data.length; index++) {
-			crc ^= (data[index] & 0xFF);
-			for (int i = 0; i < 8; ++i) {
-				boolean bit = (crc & 1) == 1;
-				crc >>= 1;
-				if (bit) crc ^= polynomial;
-			}
-
-		}
-		// Swap bytes order
-		crc = ((crc & 0xFF00) >> 8) | (crc << 8);
-		return crc & 0xFFFF;
+		return CRC(0x1021, 0x0000, data, offset, length, true, true, 0x0000);
 	}
 
 	/**
@@ -54,59 +40,122 @@ public final class CRC16 {
 	 * @return the CRC-16 CCITT-FALSE.
 	 */
 	public static int CCITT_FALSE(final @NonNull byte[] data, final int offset, final int length) {
-		int crc = 0xFFFF;
-
-		for (int i = offset; i < offset + length && i < data.length; ++i) {
-			crc = (((crc & 0xFFFF) >> 8) | (crc << 8));
-			crc ^= data[i];
-			crc ^= (crc & 0xFF) >> 4;
-			crc ^= (crc << 8) << 4;
-			crc ^= ((crc & 0xFF) << 4) << 1;
-		}
-
-		// Other implementation of the same algorithm:
-//		final int polynomial = 0x1021;
+//		Other implementation of the same algorithm:
 //		int crc = 0xFFFF;
 //
 //		for (int i = offset; i < offset + length && i < data.length; ++i) {
-//			final byte b = data[i];
-//			for (int j = 0; j < 8; j++) {
-//				boolean bit = ((b >> (7 - j) & 1) == 1);
-//				boolean c15 = ((crc >> 15 & 1) == 1);
-//				crc <<= 1;
-//				if (c15 ^ bit) crc ^= polynomial;
-//			}
+//			crc = (((crc & 0xFFFF) >> 8) | (crc << 8));
+//			crc ^= data[i];
+//			crc ^= (crc & 0xFF) >> 4;
+//			crc ^= (crc << 8) << 4;
+//			crc ^= ((crc & 0xFF) << 4) << 1;
 //		}
 
-		return crc & 0xFFFF;
+		return CRC(0x1021, 0xFFFF, data, offset, length, false, false, 0x0000);
 	}
 
 	/**
-	 * Calculates CRC CCITT-1D0F over given range of bytes from the block of data.
+	 * Calculates CRC MCRF4XX over given range of bytes from the block of data.
+	 * It is using the 0x1021 polynomial and 0xFFFF initial value.
+	 * <p>
+	 * This method is used in Bluetooth LE CGMS service E2E-CRC calculation.
+	 * <p>
+	 * See: http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-mcrf4xx<br>
+	 * See: http://ww1.microchip.com/downloads/en/AppNotes/00752a.pdf<br>
+	 * See: https://www.bluetooth.com/specifications/gatt -> CGMS (1.0.1)
+	 *
+	 * @param data   The input data block for computation.
+	 * @param offset Offset from where the range starts.
+	 * @param length Length of the range in bytes.
+	 * @return the CRC-16 MCRF4XX.
+	 */
+	public static int MCRF4XX(final @NonNull byte[] data, final int offset, final int length) {
+		return CRC(0x1021, 0xFFFF, data, offset, length, true, true, 0x0000);
+	}
+
+	/**
+	 * Calculates CRC AUG-CCITT over given range of bytes from the block of data.
 	 * It is using the 0x1021 polynomial and 0x1D0F initial value.
 	 * <p>
-	 * See: http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-ccitt-false
+	 * See: http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-aug-ccitt
 	 * See: http://srecord.sourceforge.net/crc16-ccitt.html
 	 *
 	 * @param data   The input data block for computation.
 	 * @param offset Offset from where the range starts.
 	 * @param length Length of the range in bytes.
-	 * @return the CRC-16 CCITT-1D0F.
+	 * @return the CRC-16 AUG-CCITT.
 	 */
-	public static int CCITT_1D9F(final @NonNull byte[] data, final int offset, final int length) {
-		final int polynomial = 0x1021;
-		int crc = 0x1D0F;
+	public static int AUG_CCITT(final @NonNull byte[] data, final int offset, final int length) {
+		return CRC(0x1021, 0x1D0F, data, offset, length, false, false, 0x0000);
+	}
+
+	/**
+	 * Calculates CRC-16 ARC over given range of bytes from the block of data.
+	 * It is using the 0x8005 polynomial and 0x0000 initial value.
+	 * <p>
+	 * Input data and output CRC are reversed.
+	 * <p>
+	 * See: http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-arc
+	 *
+	 * @param data   The input data block for computation.
+	 * @param offset Offset from where the range starts.
+	 * @param length Length of the range in bytes.
+	 * @return the CRC-16.
+	 */
+	public static int ARC(final @NonNull byte[] data, final int offset, final int length) {
+		return CRC(0x8005, 0x0000, data, offset, length, true, true, 0x0000);
+	}
+
+	/**
+	 * Calculates CRC-16 MAXIM over given range of bytes from the block of data.
+	 * It is using the 0x8005 polynomial and 0x0000 initial value and XORs output with 0xFFFF.
+	 * <p>
+	 * Input data and output CRC are reversed.
+	 * <p>
+	 * See: http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-maxim
+	 *
+	 * @param data   The input data block for computation.
+	 * @param offset Offset from where the range starts.
+	 * @param length Length of the range in bytes.
+	 * @return the CRC-16 MAXIM.
+	 */
+	public static int MAXIM(final @NonNull byte[] data, final int offset, final int length) {
+		return CRC(0x8005, 0x0000, data, offset, length, true, true, 0xFFFF);
+	}
+
+	/**
+	 * Calculates the CRC over given range of bytes from the block of data with given polynomial and initial value.
+	 * This method may also reverse input bytes and reverse output CRC.
+	 *
+	 * See: http://www.zorc.breitbandkatze.de/crc.html
+	 *
+	 * @param poly   Polynomial used to calculate the CRC16.
+	 * @param init   Initial value to feed the buffer.
+	 * @param data   The input data block for computation.
+	 * @param offset Offset from where the range starts.
+	 * @param length Length of the range in bytes.
+	 * @param refin  True if the input data should be reversed.
+	 * @param refout True if the output data should be reversed.
+	 * @return CRC calculated with given parameters.
+	 */
+	public static int CRC(final int poly, final int init, final @NonNull byte[] data, final int offset, final int length, final boolean refin, final boolean refout, final int xorout) {
+		int crc = init;
 
 		for (int i = offset; i < offset + length && i < data.length; ++i) {
 			final byte b = data[i];
 			for (int j = 0; j < 8; j++) {
-				boolean bit = ((b >> (7 - j) & 1) == 1);
-				boolean c15 = ((crc >> 15 & 1) == 1);
+				final int k = refin ? 7 - j : j;
+				final boolean bit = ((b >> (7 - k) & 1) == 1);
+				final boolean c15 = ((crc >> 15 & 1) == 1);
 				crc <<= 1;
-				if (c15 ^ bit) crc ^= polynomial;
+				if (c15 ^ bit) crc ^= poly;
 			}
 		}
 
-		return crc & 0xFFFF;
+		if (refout) {
+			return (Integer.reverse(crc) >>> 16) ^ xorout;
+		} else {
+			return (crc ^ xorout) & 0xFFFF;
+		}
 	}
 }
