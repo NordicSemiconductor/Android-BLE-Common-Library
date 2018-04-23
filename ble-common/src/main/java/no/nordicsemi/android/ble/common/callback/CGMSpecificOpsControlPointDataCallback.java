86b43f0087ddb59d6a823f19fdd8b851af89ad04
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-04-23 12:44:17 +0200

    CGM Specific Ops Control Point callbacks

---
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/CGMSpecificOpsControlPointDataCallback.java
@@ -0,0 +1,132 @@
+package no.nordicsemi.android.ble.common.callback;
+
+import android.bluetooth.BluetoothDevice;
+import android.support.annotation.NonNull;
+
+import no.nordicsemi.android.ble.callback.profile.ProfileDataCallback;
+import no.nordicsemi.android.ble.common.profile.CGMSpecificOpsControlPointCallback;
+import no.nordicsemi.android.ble.common.util.CRC16;
+import no.nordicsemi.android.ble.data.Data;
+
+@SuppressWarnings("ConstantConditions")
+public abstract class CGMSpecificOpsControlPointDataCallback implements ProfileDataCallback, CGMSpecificOpsControlPointCallback {
+	private final static int OP_CODE_COMMUNICATION_INTERVAL_RESPONSE = 3;
+	private final static int OP_CODE_CALIBRATION_VALUE_RESPONSE = 6;
+	private final static int OP_CODE_PATIENT_HIGH_ALERT_LEVEL_RESPONSE = 9;
+	private final static int OP_CODE_PATIENT_LOW_ALERT_LEVEL_RESPONSE = 12;
+	private final static int OP_CODE_HYPO_ALERT_LEVEL_RESPONSE = 15;
+	private final static int OP_CODE_HYPER_ALERT_LEVEL_RESPONSE = 18;
+	private final static int OP_CODE_RATE_OF_DECREASE_ALERT_LEVEL_RESPONSE = 21;
+	private final static int OP_CODE_RATE_OF_INCREASE_ALERT_LEVEL_RESPONSE = 24;
+	private final static int OP_CODE_RESPONSE_CODE = 28;
+	private final static int CGM_RESPONSE_SUCCESS = 1;
+
+	@Override
+	public void onDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+		if (data.size() < 2) {
+			onInvalidDataReceived(device, data);
+			return;
+		}
+
+		// Read the Op Code
+		final int opCode = data.getIntValue(Data.FORMAT_UINT8, 0);
+
+		// Estimate the expected operand size based on the Op Code
+		int expectedOperandSize;
+		switch (opCode) {
+			case OP_CODE_COMMUNICATION_INTERVAL_RESPONSE:
+				// UINT8
+				expectedOperandSize = 1;
+				break;
+			case OP_CODE_CALIBRATION_VALUE_RESPONSE:
+				// Calibration Value
+				expectedOperandSize = 10;
+				break;
+			case OP_CODE_PATIENT_HIGH_ALERT_LEVEL_RESPONSE:
+			case OP_CODE_PATIENT_LOW_ALERT_LEVEL_RESPONSE:
+			case OP_CODE_HYPO_ALERT_LEVEL_RESPONSE:
+			case OP_CODE_HYPER_ALERT_LEVEL_RESPONSE:
+			case OP_CODE_RATE_OF_DECREASE_ALERT_LEVEL_RESPONSE:
+			case OP_CODE_RATE_OF_INCREASE_ALERT_LEVEL_RESPONSE:
+				// SFLOAT
+				expectedOperandSize = 2;
+				break;
+			case OP_CODE_RESPONSE_CODE:
+				// Request Op Code (UINT8), Response Code Value (UINT8)
+				expectedOperandSize = 2;
+				break;
+			default:
+				onInvalidDataReceived(device, data);
+				return;
+		}
+
+		// Verify packet length
+		if (data.size() != 1 + expectedOperandSize && data.size() != 1 + expectedOperandSize + 2) {
+			onInvalidDataReceived(device, data);
+			return;
+		}
+
+		// Verify CRC if present
+		final boolean crcPresent = data.size() == 1 + expectedOperandSize + 2; // opCode + expected operand + CRC
+		if (crcPresent) {
+			final int expectedCrc = data.getIntValue(Data.FORMAT_UINT16, 1 + expectedOperandSize);
+			final int actualCrc   = CRC16.MCRF4XX(data.getValue(), 0, 1 + expectedOperandSize);
+			if (expectedCrc != actualCrc) {
+				onCGMSpecificOpsResponseReceivedWithCrcError(data);
+				return;
+			}
+		}
+
+		switch (opCode) {
+			case OP_CODE_COMMUNICATION_INTERVAL_RESPONSE:
+				final int interval = data.getIntValue(Data.FORMAT_UINT8, 1);
+				onContinuousGlucoseCommunicationIntervalReceived(interval, crcPresent);
+				break;
+			case OP_CODE_CALIBRATION_VALUE_RESPONSE:
+				final float glucoseConcentrationOfCalibration = data.getFloatValue(Data.FORMAT_SFLOAT, 1);
+				final int calibrationTime = data.getIntValue(Data.FORMAT_UINT16, 3);
+				final int calibrationTypeAndSampleLocation = data.getIntValue(Data.FORMAT_UINT8, 5);
+				final int calibrationType = calibrationTypeAndSampleLocation & 0x0F;
+				final int calibrationSampleLocation = calibrationTypeAndSampleLocation >> 4;
+				final int nextCalibrationTime = data.getIntValue(Data.FORMAT_UINT16, 6);
+				final int calibrationDataRecordNumber = data.getIntValue(Data.FORMAT_UINT16, 8);
+				final int calibrationStatus = data.getIntValue(Data.FORMAT_UINT8, 10);
+				onContinuousGlucoseCalibrationValueReceived(glucoseConcentrationOfCalibration,
+						calibrationTime, nextCalibrationTime, calibrationType, calibrationSampleLocation,
+						calibrationDataRecordNumber, new CGMCalibrationStatus(calibrationStatus), crcPresent);
+				break;
+			case OP_CODE_RESPONSE_CODE:
+				// final int requestCode = data.getIntValue(Data.FORMAT_UINT8, 1); // ignore
+				final int responseCode = data.getIntValue(Data.FORMAT_UINT8, 2);
+				if (responseCode == CGM_RESPONSE_SUCCESS) {
+					onCGMSpecificOpsOperationCompleted();
+				} else {
+					onCGMSpecificOpsOperationError(responseCode);
+				}
+				break;
+		}
+
+		// Read SFLOAT value
+		final float value = data.getFloatValue(Data.FORMAT_SFLOAT, 1);
+		switch (opCode) {
+			case OP_CODE_PATIENT_HIGH_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucosePatientHighAlertReceived(value, crcPresent);
+				break;
+			case OP_CODE_PATIENT_LOW_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucosePatientLowAlertReceived(value, crcPresent);
+				break;
+			case OP_CODE_HYPO_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucoseHypoAlertReceived(value, crcPresent);
+				break;
+			case OP_CODE_HYPER_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucoseHyperAlertReceived(value, crcPresent);
+				break;
+			case OP_CODE_RATE_OF_DECREASE_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucoseRateOfDecreaseAlertReceived(value, crcPresent);
+				break;
+			case OP_CODE_RATE_OF_INCREASE_ALERT_LEVEL_RESPONSE:
+				onContinuousGlucoseRateOfIncreaseAlertReceived(value, crcPresent);
+				break;
+		}
+	}
+}
---
+++ ble-common/src/test/java/no/nordicsemi/android/ble/common/callback/CGMSpecificOpsControlPointDataCallbackTest.java
@@ -0,0 +1,240 @@
+package no.nordicsemi.android.ble.common.callback;
+
+import android.bluetooth.BluetoothDevice;
+import android.support.annotation.NonNull;
+
+import org.junit.Test;
+
+import no.nordicsemi.android.ble.callback.DataCallback;
+import no.nordicsemi.android.ble.common.profile.ContinuousGlucoseMeasurementTypes;
+import no.nordicsemi.android.ble.data.Data;
+
+import static org.junit.Assert.*;
+
+public class CGMSpecificOpsControlPointDataCallbackTest {
+	private boolean success;
+	private boolean invalidCrc;
+	private boolean invalidData;
+	private boolean valueReceived;
+	private boolean secured;
+	private int error;
+	private int interval;
+	private float patientHighAlertLevel;
+	private float patientLowAlertLevel;
+	private float hypoAlertLevel;
+	private float hyperAlertLevel;
+	private float rateOfDecreaseAlertLevel;
+	private float rateOfIncreaseAlertLevel;
+
+	private final DataCallback callback = new CGMSpecificOpsControlPointDataCallback() {
+		@Override
+		public void onDataReceived(@NonNull final BluetoothDevice device, @NonNull final Data data) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.success = false;
+			CGMSpecificOpsControlPointDataCallbackTest.this.invalidCrc = false;
+			CGMSpecificOpsControlPointDataCallbackTest.this.invalidData = false;
+			CGMSpecificOpsControlPointDataCallbackTest.this.valueReceived = false;
+			CGMSpecificOpsControlPointDataCallbackTest.this.error = 0;
+			CGMSpecificOpsControlPointDataCallbackTest.this.interval = 0;
+			CGMSpecificOpsControlPointDataCallbackTest.this.patientHighAlertLevel = 0.0f;
+			CGMSpecificOpsControlPointDataCallbackTest.this.patientLowAlertLevel = 0.0f;
+			CGMSpecificOpsControlPointDataCallbackTest.this.hypoAlertLevel = 0.0f;
+			CGMSpecificOpsControlPointDataCallbackTest.this.hyperAlertLevel = 0.0f;
+			CGMSpecificOpsControlPointDataCallbackTest.this.rateOfDecreaseAlertLevel = 0.0f;
+			CGMSpecificOpsControlPointDataCallbackTest.this.rateOfIncreaseAlertLevel = 0.0f;
+			super.onDataReceived(device, data);
+		}
+
+		@Override
+		public void onCGMSpecificOpsOperationCompleted() {
+			CGMSpecificOpsControlPointDataCallbackTest.this.success = true;
+		}
+
+		@Override
+		public void onCGMSpecificOpsOperationError(final int error) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.error = error;
+		}
+
+		@Override
+		public void onCGMSpecificOpsResponseReceivedWithCrcError(@NonNull final Data data) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.invalidCrc = true;
+		}
+
+		@Override
+		public void onContinuousGlucoseCommunicationIntervalReceived(final int interval, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.interval = interval;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucoseCalibrationValueReceived(final float glucoseConcentrationOfCalibration,
+																final int calibrationTime, final int nextCalibrationTime,
+																final int type, final int sampleLocation,
+																final int calibrationDataRecordNumber,
+																final CGMCalibrationStatus status, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.valueReceived = true;
+			assertEquals("Glucose concentration of calibration", 100.0f, glucoseConcentrationOfCalibration, 0.01);
+			assertEquals("Calibration time", 10, calibrationTime);
+			assertEquals("Calibration time", 20, nextCalibrationTime);
+			assertEquals("Type", TYPE_CAPILLARY_PLASMA, type);
+			assertEquals("Sample location", SAMPLE_LOCATION_EARLOBE, sampleLocation);
+			assertEquals("Data record Number", 1, calibrationDataRecordNumber);
+			assertNotNull(status);
+			assertFalse(status.rejected);
+			assertFalse(status.dataOutOfRange);
+			assertTrue(status.processPending);
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucosePatientHighAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.patientHighAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucosePatientLowAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.patientLowAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucoseHypoAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.hypoAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucoseHyperAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.hyperAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucoseRateOfDecreaseAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.rateOfDecreaseAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+
+		@Override
+		public void onContinuousGlucoseRateOfIncreaseAlertReceived(final float alertLevel, final boolean secured) {
+			CGMSpecificOpsControlPointDataCallbackTest.this.rateOfIncreaseAlertLevel = alertLevel;
+			CGMSpecificOpsControlPointDataCallbackTest.this.secured = secured;
+		}
+	};
+
+	@Test
+	public void onCGMSpecificOpsOperationCompleted() {
+		final Data data = new Data(new byte[] { 28, 2, 1});
+		callback.onDataReceived(null, data);
+		assertTrue(success);
+	}
+
+	@Test
+	public void onCGMSpecificOpsOperationError() {
+		final Data data = new Data(new byte[] { 28, 2, 2});
+		callback.onDataReceived(null, data);
+		assertEquals(error, 2);
+	}
+
+	@Test
+	public void onCGMSpecificOpsOperationCompleted_withCrc() {
+		final Data data = new Data(new byte[] { 28, 2, 1, (byte) 0x3C, (byte) 0x3B});
+		callback.onDataReceived(null, data);
+		assertTrue(success);
+	}
+
+	@Test
+	public void onCGMSpecificOpsOperationError_withCrc() {
+		final Data data = new Data(new byte[] { 28, 2, 2, (byte) 0xA7, (byte) 0x09});
+		callback.onDataReceived(null, data);
+		assertEquals(error, 2);
+	}
+
+	@Test
+	public void onCGMSpecificOpsResponseReceivedWithCrcError() {
+		final Data data = new Data(new byte[] { 28, 2, 2, (byte) 0xA7, (byte) 0x01});
+		callback.onDataReceived(null, data);
+		assertTrue(invalidCrc);
+	}
+
+	@Test
+	public void onContinuousGlucoseCalibrationValueReceived() {
+		final Data data = new Data(new byte[11]);
+		data.setValue(6, Data.FORMAT_UINT8, 0);
+		data.setValue(1, 2, Data.FORMAT_SFLOAT, 1);
+		data.setValue(10, Data.FORMAT_UINT16, 3);
+		data.setValue(0x32, Data.FORMAT_UINT8, 5);
+		data.setValue(20, Data.FORMAT_UINT16, 6);
+		data.setValue(1, Data.FORMAT_UINT16, 8);
+		data.setValue(0b100, Data.FORMAT_UINT8, 10);
+
+		callback.onDataReceived(null, data);
+		assertTrue(valueReceived);
+		assertFalse(secured);
+	}
+
+	@Test
+	public void onContinuousGlucoseCalibrationValueReceived_withCrc() {
+		final Data data = new Data(new byte[13]);
+		data.setValue(6, Data.FORMAT_UINT8, 0);
+		data.setValue(1, 2, Data.FORMAT_SFLOAT, 1);
+		data.setValue(10, Data.FORMAT_UINT16, 3);
+		data.setValue(0x32, Data.FORMAT_UINT8, 5);
+		data.setValue(20, Data.FORMAT_UINT16, 6);
+		data.setValue(1, Data.FORMAT_UINT16, 8);
+		data.setValue(0b100, Data.FORMAT_UINT8, 10);
+		data.setValue(0xB2BF, Data.FORMAT_UINT16, 11);
+
+		callback.onDataReceived(null, data);
+		assertTrue(valueReceived);
+		assertTrue(secured);
+	}
+
+	@Test
+	public void onContinuousGlucosePatientHighAlertReceived() {
+		final Data data = new Data(new byte[] { 9, 12, -16});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 1.2f, patientHighAlertLevel, 0.01);
+		assertFalse(secured);
+	}
+
+	@Test
+	public void onContinuousGlucosePatientLowAlertReceived() {
+		final Data data = new Data(new byte[] { 12, 11, (byte) 0b111000000, (byte) 0x34, (byte) 0xBE});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 0.0011f, patientLowAlertLevel, 0.0001);
+		assertTrue(secured);
+	}
+
+	@Test
+	public void onContinuousGlucoseHypoAlertReceived() {
+		final Data data = new Data(new byte[] { 15, 1, 0});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 1.0f, hypoAlertLevel, 0.00);
+		assertFalse(secured);
+	}
+
+	@Test
+	public void onContinuousGlucoseHyperAlertReceived() {
+		final Data data = new Data(new byte[] { 18, 10, 32});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 1000f, hyperAlertLevel, 0.01);
+		assertFalse(secured);
+	}
+
+	@Test
+	public void onContinuousGlucoseRateOfDecreaseAlertReceived() {
+		final Data data = new Data(new byte[] { 21, 1, 16});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 10.0f, rateOfDecreaseAlertLevel, 0.00);
+		assertFalse(secured);
+	}
+
+	@Test
+	public void onContinuousGlucoseRateOfIncreaseAlertReceived() {
+		final Data data = new Data(new byte[] { 24, 10, 64});
+		callback.onDataReceived(null, data);
+		assertEquals("Level", 100000f, rateOfIncreaseAlertLevel, 0.01);
+		assertFalse(secured);
+	}
+}
