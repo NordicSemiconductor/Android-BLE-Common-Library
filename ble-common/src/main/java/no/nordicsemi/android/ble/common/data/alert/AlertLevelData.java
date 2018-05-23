5a2c6f25b4421f486882c4d47b10cfe94758e936
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-05-23 16:06:46 +0200

    Comments added to AlertLevelData

--- ble-common/src/main/java/no/nordicsemi/android/ble/common/data/alert/AlertLevelData.java
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/data/alert/AlertLevelData.java
@@ -8,14 +8,23 @@ public final class AlertLevelData {
 	private static final byte[] MILD_ALERT = { 0x01 };
 	private static final byte[] NO_ALERT = { 0x00 };
 
+	/**
+	 * Disables alert.
+	 */
 	public static Data noAlert() {
 		return new Data(NO_ALERT);
 	}
 
+	/**
+	 * Sets the alert level to MILD.
+	 */
 	public static Data mildAlert() {
 		return new Data(MILD_ALERT);
 	}
 
+	/**
+	 * Sets the alert level to HIGH.
+	 */
 	public static Data highAlert() {
 		return new Data(HIGH_ALERT);
 	}
